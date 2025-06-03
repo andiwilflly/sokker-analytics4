66825060f6ba09b8b62e0276f9ea68ab6041b757
volodymyr <volodymyr.sedikh@axonops.com>
2025-06-03 16:21:02 +0300

    324

--- apps/client/src/components/transfers/dashboard/TransfersDashboardGrid.component.tsx
+++ apps/client/src/components/transfers/dashboard/TransfersDashboardGrid.component.tsx
@@ -33,24 +33,30 @@ class TransfersDashboardGrid extends React.Component<IProps> {
 	render() {
 		return (
 			<div className="z-10 relative :grid">
-				<ResponsiveGridLayout
-					draggableHandle=".\:grid-draggable-handler"
-					onLayoutChange={this.onLayoutChange}
-					layout={store.transfers.grid.layout}
-					cols={12}
-					margin={[10, 10]}
-					containerPadding={[0, 0]}
-					rowHeight={30}>
-					{store.transfers.grid.layout.map(item => {
-						return (
-							<div key={item.i}>
-								<AutoSizer>
-									{({ width, height }) => <TransfersDashboardItem i={item.i} width={width} height={height} />}
-								</AutoSizer>
-							</div>
-						);
-					})}
-				</ResponsiveGridLayout>
+				{store.transfers.grid.layout.length ? (
+					<ResponsiveGridLayout
+						draggableHandle=".\:grid-draggable-handler"
+						onLayoutChange={this.onLayoutChange}
+						layout={store.transfers.grid.layout}
+						cols={12}
+						margin={[10, 10]}
+						containerPadding={[0, 0]}
+						rowHeight={30}>
+						{store.transfers.grid.layout.map(item => {
+							return (
+								<div key={item.i}>
+									<AutoSizer>
+										{({ width, height }) => <TransfersDashboardItem i={item.i} width={width} height={height} />}
+									</AutoSizer>
+								</div>
+							);
+						})}
+					</ResponsiveGridLayout>
+				) : (
+					<div className="p-10 bg-gray-200 rounded text-gray-500 text-xs flex justify-center">
+						<T>Add new widget to analyze transfers data</T>
+					</div>
+				)}
 
 				<div className="flex justify-end mt-2 mb-2">
 					<button className=":button" onClick={this.onAddNewWidget}>
